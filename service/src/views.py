e8735587c5cac1e8726387e28f3405b6997c5f3f
a-miscellaneous <96189996+a-miscellaneous@users.noreply.github.com>
2024-05-29 21:09:50 +0200

    added page scrolling

--- service/src/views.py
+++ service/src/views.py
@@ -15,26 +15,32 @@ async def home():
 
 @views.route('/page_<int:number>', methods=['GET'])
 async def page(number):
+    IMAGES_PER_PAGE = 5
     all_images = ENOFT.query.all()
     all_images.sort(key=lambda x: x.creation_date, reverse=True)
     if not all_images:
         logger.error("No images found in database.")
         all_images = []
-    all_images = [x.image_path for x in all_images]
+    all_images = [(x.image_path, x.owner_email) for x in all_images]
     # check limits
     if number < 1:
         flash('No such page', 'error')
         number = 1
-
-    if number > len(all_images) // 10 + 1:
+    last_page = (len(all_images) // IMAGES_PER_PAGE) + 1
+    if number > last_page:
         flash('No such page', 'error')
-        number = len(all_images) // 10 + 1
+        number = last_page
 
     # get images
-    images = all_images[(number - 1) * 10:number * 10]
+    images = all_images[(number - 1) *
+                        IMAGES_PER_PAGE:number * IMAGES_PER_PAGE]
 
     return render_template(
         "home.html",
         user=current_user,
         images=images,
-        page=number)
+        page=number,
+        max_pages=last_page,
+        min_page=max(1, number - 5),
+        max_page=min(last_page, number + 5)
+    )
