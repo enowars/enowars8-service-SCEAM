b5c3da541654a70c42812a04660492c282f767c9
a-miscellaneous <96189996+a-miscellaneous@users.noreply.github.com>
2024-06-05 13:05:38 +0200

    addes registration stress test and reduced key size for speed up

---
+++ checker/stress_test/registration.py
@@ -0,0 +1,114 @@
+import random
+from requests import Session
+import string
+import threading
+import time
+import asyncio
+
+address = "http://localhost:8008/"
+THREADS = 100
+
+# timing decorator
+
+
+def timing(f):
+    def wrap(*args):
+        time1 = time.time()
+        ret = f(*args)
+        time2 = time.time()
+        diff = time2-time1
+        print(f'{f.__name__} took {diff:.2f} s')
+        print(f'{f.__name__} took {diff/THREADS:.5f} s per thread')
+        return ret
+    return wrap
+
+
+def generate_random_string(length):
+    return ''.join(
+        random.choices(
+            string.ascii_letters +
+            string.digits,
+            k=length))
+
+
+def register():
+    name = generate_random_string(20)
+    email = name + "@" + generate_random_string(20) + ".scam"
+    data = {'email': email, 'name': name}
+    data['vendor_lock'] = 'on'
+    data['never_full'] = 'on'
+
+    client = Session()
+    try:
+        r = client.post(address + 'sign-up', data=data)
+
+    except:
+        print("registration failed")
+
+    try:
+        r = client.post(address + 'download_key')
+    except:
+        print("download key failed")
+
+    key = r.content
+    return key
+
+
+async def register_async():
+    name = generate_random_string(20)
+    email = name + "@" + generate_random_string(20) + ".scam"
+    data = {'email': email, 'name': name}
+    data['vendor_lock'] = 'on'
+    data['never_full'] = 'on'
+
+    client = Session()
+    try:
+        r = client.post(address + 'sign-up', data=data)
+
+    except:
+        print("registration failed")
+
+    try:
+        r = client.post(address + 'download_key')
+    except:
+        print("download key failed")
+
+    key = r.content
+    return key
+
+
+@timing
+def create_threads(threads):
+    for i in range(THREADS):
+        t = threading.Thread(target=register)
+        threads.append(t)
+
+
+@timing
+def start_threads(threads):
+    for t in threads:
+        t.start()
+
+
+@timing
+def join_threads(threads):
+    for t in threads:
+        t.join()
+
+
+@timing
+def asyncio_register():
+    for i in range(THREADS):
+        asyncio.run(register_async())
+
+
+def stress():
+    threads = []
+    asyncio_register()
+    create_threads(threads)
+    start_threads(threads)
+    join_threads(threads)
+
+
+if __name__ == "__main__":
+    stress()
--- service/src/auth.py
+++ service/src/auth.py
@@ -127,7 +127,7 @@ async def sign_up():
 
 
 def generate_keys():
-    key = rsa.generate_private_key(public_exponent=65537, key_size=2048)
+    key = rsa.generate_private_key(public_exponent=65537, key_size=512)
     public_key = key.public_key().public_bytes(
         encoding=serialization.Encoding.PEM,
         format=serialization.PublicFormat.SubjectPublicKeyInfo
