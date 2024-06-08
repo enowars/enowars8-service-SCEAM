25010146d22aa91081f15453dfa79339c1eebbeb
a-miscellaneous <96189996+a-miscellaneous@users.noreply.github.com>
2024-06-08 17:47:07 +0200

    removed async from stress test

--- checker/stress_test/registration.py
+++ checker/stress_test/registration.py
@@ -3,7 +3,6 @@ from requests import Session
 import string
 import threading
 import time
-import asyncio
 
 address = "http://localhost:8008/"
 THREADS = 100
@@ -17,8 +16,8 @@ def timing(f):
         ret = f(*args)
         time2 = time.time()
         diff = time2-time1
-        # print(f'{f.__name__} took {diff:.2f} s')
-        # print(f'{f.__name__} took {diff/THREADS:.5f} s per thread')
+        print(f'{f.__name__} took {diff:.2f} s')
+        print(f'{f.__name__} took {diff/THREADS:.5f} s per thread')
         return ret
     return wrap
 
@@ -32,29 +31,6 @@ def generate_random_string(length):
 
 
 def register():
-    name = generate_random_string(20)
-    email = name + "@" + generate_random_string(20) + ".scam"
-    data = {'email': email, 'name': name}
-    data['vendor_lock'] = 'on'
-    data['never_full'] = 'on'
-
-    client = Session()
-    try:
-        r = client.post(address + 'sign-up', data=data)
-
-    except:
-        print("registration failed")
-
-    try:
-        r = client.post(address + 'download_key')
-    except:
-        print("download key failed")
-
-    key = r.content
-    return key
-
-
-async def register_async():
     name = generate_random_string(20)
     email = name + "@" + generate_random_string(20) + ".scam"
     data = {'email': email, 'name': name}
@@ -97,19 +73,12 @@ def join_threads(threads):
 
 
 @timing
-def asyncio_register():
-    for i in range(THREADS):
-        asyncio.run(register_async())
-
-
 def stress():
     threads = []
-    asyncio_register()
     create_threads(threads)
     start_threads(threads)
     join_threads(threads)
 
 
 if __name__ == "__main__":
-    while (True):
-        stress()
+    stress()
