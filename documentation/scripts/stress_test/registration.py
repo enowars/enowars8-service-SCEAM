c75e06d950adfc4ada7e669becaf14f649de2ad5
a-miscellaneous <96189996+a-miscellaneous@users.noreply.github.com>
2024-06-15 17:19:15 +0200

    figured out that async Client is unstable but still decently quick

--- documentation/scripts/stress_test/registration.py
+++ documentation/scripts/stress_test/registration.py
@@ -3,11 +3,11 @@ from requests import Session
 import string
 import threading
 import time
+from httpx import AsyncClient
+import asyncio
 
 address = "http://localhost:8008/"
 THREADS = 100
-
-# timing decorator
 
 
 def timing(f):
@@ -30,55 +30,70 @@ def generate_random_string(length):
             k=length))
 
 
-def register():
-    name = generate_random_string(20)
-    email = name + "@" + generate_random_string(20) + ".scam"
+def generate_random_data(length):
+    name = generate_random_string(length)
+    email = name + "@" + generate_random_string(length) + ".scam"
     data = {'email': email, 'name': name}
     data['vendor_lock'] = 'on'
     data['never_full'] = 'on'
+    return data
 
+
+def register_sessions():
+    data = generate_random_data(10)
     client = Session()
     try:
         r = client.post(address + 'sign-up', data=data)
-
-    except:
-        print("registration failed")
-
-    try:
         r = client.post(address + 'download_key')
     except:
-        print("download key failed")
+        print("registration failed")
+    key = r.content
+    return key
 
+
+async def register_asyncClient():
+    data = generate_random_data(10)
+    client = AsyncClient()
+    r = None
+    try:
+        while r is None:
+            r = await client.post(address + 'sign-up', data=data)
+        r = None
+        while r is None:
+            r = await client.post(address + 'download_key')
+    except:
+        print("registration failed")
+    if not r:
+        print("response is None")
+        return None
     key = r.content
     return key
 
 
 @timing
-def create_threads(threads):
-    for i in range(THREADS):
-        t = threading.Thread(target=register)
-        threads.append(t)
-
-
-@timing
-def start_threads(threads):
+def stress_sessions():
+    threads = []
+    for _ in range(THREADS):
+        threads.append(threading.Thread(target=register_sessions))
     for t in threads:
         t.start()
-
-
-@timing
-def join_threads(threads):
     for t in threads:
         t.join()
 
 
 @timing
-def stress():
+def stress_asyncClient():
     threads = []
-    create_threads(threads)
-    start_threads(threads)
-    join_threads(threads)
+    for _ in range(THREADS):
+        threads.append(threading.Thread(
+            target=asyncio.run, args=(register_asyncClient(),)))
+    for t in threads:
+        t.start()
+    for t in threads:
+        t.join()
 
 
+# WARNING retryies infinite times
 if __name__ == "__main__":
-    stress()
+    stress_sessions()
+    stress_asyncClient()
