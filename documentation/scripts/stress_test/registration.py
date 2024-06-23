0cc4970538452c931e501acec71ec72825390b5f
a-miscellaneous <96189996+a-miscellaneous@users.noreply.github.com>
2024-06-23 21:57:28 +0200

    tmp

--- documentation/scripts/stress_test/registration.py
+++ documentation/scripts/stress_test/registration.py
@@ -1,10 +1,11 @@
 import random
-from requests import Session
 import string
 import threading
 import time
-from httpx import AsyncClient
+import requests
 import asyncio
+from httpx import AsyncClient
+import aiohttp
 
 address = "http://localhost:8008/"
 THREADS = 100
@@ -15,7 +16,7 @@ def timing(f):
         time1 = time.time()
         ret = f(*args)
         time2 = time.time()
-        diff = time2-time1
+        diff = time2 - time1
         print(f'{f.__name__} took {diff:.2f} s')
         print(f'{f.__name__} took {diff/THREADS:.5f} s per thread')
         return ret
@@ -23,16 +24,12 @@ def timing(f):
 
 
 def generate_random_string(length):
-    return ''.join(
-        random.choices(
-            string.ascii_letters +
-            string.digits,
-            k=length))
+    return ''.join(random.choices(string.ascii_letters + string.digits, k=length))
 
 
 def generate_random_data(length):
     name = generate_random_string(length)
-    email = name + "@" + generate_random_string(length) + ".scam"
+    email = f"{name}@{generate_random_string(length)}.scam"
     data = {'email': email, 'name': name}
     data['vendor_lock'] = 'on'
     data['never_full'] = 'on'
@@ -41,33 +38,12 @@ def generate_random_data(length):
 
 def register_sessions():
     data = generate_random_data(10)
-    client = Session()
+    client = requests.Session()
     try:
-        r = client.post(address + 'sign-up', data=data)
-        r = client.post(address + 'download_key')
-    except:
-        print("registration failed")
-    key = r.content
-    return key
-
-
-async def register_asyncClient():
-    data = generate_random_data(10)
-    client = AsyncClient()
-    r = None
-    try:
-        while r is None:
-            r = await client.post(address + 'sign-up', data=data)
-        r = None
-        while r is None:
-            r = await client.post(address + 'download_key')
-    except:
-        print("registration failed")
-    if not r:
-        print("response is None")
-        return None
-    key = r.content
-    return key
+        client.post(address + 'sign-up', data=data)
+        client.post(address + 'download_key')
+    except Exception as e:
+        print(f"registration failed {e}")
 
 
 @timing
@@ -81,19 +57,44 @@ def stress_sessions():
         t.join()
 
 
+async def register_asyncClient(session, endpoint):
+    data = generate_random_data(10)
+    try:
+        async with session.post(address + endpoint, data=data) as response:
+            response_data = await response.text()
+    except Exception as e:
+        print(f"Request to {endpoint} failed: {e}")
+
+
+async def stress_aiohttp():
+    tasks = []
+    async with aiohttp.ClientSession() as session:
+        for _ in range(THREADS):
+            tasks.append(register_asyncClient(session, 'sign-up'))
+            tasks.append(register_asyncClient(session, 'download_key'))
+        await asyncio.gather(*tasks)
+
+
 @timing
-def stress_asyncClient():
-    threads = []
-    for _ in range(THREADS):
-        threads.append(threading.Thread(
-            target=asyncio.run, args=(register_asyncClient(),)))
-    for t in threads:
-        t.start()
-    for t in threads:
-        t.join()
+def run_async_aiohttp():
+    asyncio.run(stress_aiohttp())
 
 
-# WARNING retryies infinite times
+async def stress_asyncClient():
+    tasks = []
+    async with AsyncClient() as client:
+        for _ in range(THREADS):
+            tasks.append(register_asyncClient(client, 'sign-up'))
+            tasks.append(register_asyncClient(client, 'download_key'))
+        await asyncio.gather(*tasks)
+
+
+@timing
+def run_async_asyncclient():
+    asyncio.run(stress_asyncClient())
+
+
 if __name__ == "__main__":
     stress_sessions()
-    stress_asyncClient()
+    run_async_aiohttp()
+    run_async_asyncclient()
